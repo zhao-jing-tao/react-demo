c1872c70144fc5fb6564851f0c0d8fc5a0684d60
zhao-jing-tao <1578249179@qq.com>
2023-03-15 18:11:55 +0800

    全局样式及组件内部样式

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Button } from "antd";
+import styled from "styled-components";
+const StyledDiv = styled.div`
+color: red;
+font-size: 20px;
+`
 function App() {
   return (
     <div className="App">
       React-app-template
       <h1>测试demohaha </h1>
       <Button>测试antd组件</Button>
+      <StyledDiv>样式测试</StyledDiv>
     </div>
   );
 }
