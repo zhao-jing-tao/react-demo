b8e946b44df3c957b313ed05ab5821f272909685
zhao-jing-tao <1578249179@qq.com>
2023-03-15 17:28:54 +0800

    webpack配置

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import { Button } from "antd";
 function App() {
   return (
     <div className="App">
       React-app-template
       <h1>测试demohaha </h1>
+      <Button>测试antd组件</Button>
     </div>
   );
 }
